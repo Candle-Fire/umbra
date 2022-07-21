3f064d51d199f6116779bdf9e115d876ad35939f
Curle <curle@gemwire.uk>
2022-07-21 20:34:43 +0100

    Implement ImGui

--- projs/shadow/shadow-engine/src/core/ShadowApplication.cpp
+++ projs/shadow/shadow-engine/src/core/ShadowApplication.cpp
@@ -1,11 +1,19 @@
 #include "ShadowApplication.h"
 
 #include "Time.h"
+#include "imgui.h"
+#include "imgui_impl_vulkan.h"
+#include "imgui_impl_sdl.h"
 #include <vlkx/vulkan/VulkanManager.h>
 #include <vlkx/render/Camera.h>
 #include <vlkx/render/geometry/SingleRenderer.h>
 
+
 namespace ShadowEngine {
+
+    // Create the renderer
+    SingleRenderer object;
+
 
 	ShadowApplication* ShadowApplication::instance = nullptr;
 	
@@ -51,6 +59,99 @@ namespace ShadowEngine {
 
         VulkanManager::getInstance()->initVulkan(window_->sdlWindowPtr);
 
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+        // Setup Dear ImGui style
+        ImGui::StyleColorsDark();
+
+
+        VkDescriptorPool imGuiPool;
+        VulkanManager* vk = VulkanManager::getInstance();
+        VkDescriptorPoolSize pool_sizes[] =
+                {
+                        { VK_DESCRIPTOR_TYPE_SAMPLER, 1000 },
+                        { VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1000 },
+                        { VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, 1000 },
+                        { VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, 1000 },
+                        { VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER, 1000 },
+                        { VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER, 1000 },
+                        { VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1000 },
+                        { VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1000 },
+                        { VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, 1000 },
+                        { VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC, 1000 },
+                        { VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT, 1000 }
+                };
+
+        VkDescriptorPoolCreateInfo pool_info = {};
+        pool_info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+        pool_info.flags = VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT;
+        pool_info.maxSets = 1000 * IM_ARRAYSIZE(pool_sizes);
+        pool_info.poolSizeCount = (uint32_t)IM_ARRAYSIZE(pool_sizes);
+        pool_info.pPoolSizes = pool_sizes;
+        vkCreateDescriptorPool(vk->getDevice()->logical, &pool_info, VK_NULL_HANDLE, &imGuiPool);
+
+        // Setup Platform/Renderer backends
+        ImGui_ImplSDL2_InitForVulkan(window_->sdlWindowPtr);
+        ImGui_ImplVulkan_InitInfo init_info = {};
+        init_info.Instance = vk->getVulkan();
+        init_info.PhysicalDevice = vk->getDevice()->physical;
+        init_info.Device = vk->getDevice()->logical;
+        init_info.QueueFamily = vk->getDevice()->queueData.graphics;
+        init_info.Queue = vk->getDevice()->graphicsQueue;
+        init_info.PipelineCache = VK_NULL_HANDLE;
+        init_info.DescriptorPool = imGuiPool;
+        init_info.Subpass = 0;
+        init_info.MinImageCount = vk->getSwapchain()->images.size();
+        init_info.ImageCount = vk->getSwapchain()->images.size();
+        init_info.MSAASamples = VK_SAMPLE_COUNT_1_BIT;
+        init_info.Allocator = VK_NULL_HANDLE;
+        init_info.CheckVkResultFn = nullptr;
+        ImGui_ImplVulkan_Init(&init_info, vk->getRenderPass()->pass);
+
+        // Load Fonts
+        // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+        // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
+        // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+        // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+        // - Read 'docs/FONTS.md' for more instructions and details.
+        // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+        //io.Fonts->AddFontDefault();
+        //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+        //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+        //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+        //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
+        //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+        //IM_ASSERT(font != NULL);
+
+        // Upload Fonts
+        {
+            // Prepare to create a temporary command pool.
+            VkCommandPool pool;
+            VkCommandPoolCreateInfo poolCreateInfo = {};
+            poolCreateInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+            poolCreateInfo.queueFamilyIndex = vk->getDevice()->queueData.graphics;
+            poolCreateInfo.flags = 0;
+
+            // Create the pool
+            if (vkCreateCommandPool(vk->getDevice()->logical, &poolCreateInfo, nullptr, &pool) != VK_SUCCESS)
+                throw std::runtime_error("Unable to allocate a temporary command pool");
+
+            VkCommandBuffer buffer = VkTools::createTempCommandBuffer(pool, vk->getDevice()->logical);
+
+            ImGui_ImplVulkan_CreateFontsTexture(buffer);
+
+            VkSubmitInfo end_info = {};
+            end_info.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+            end_info.commandBufferCount = 1;
+            end_info.pCommandBuffers = &buffer;
+
+            VkTools::executeAndDeleteTempBuffer(buffer, pool, vk->getDevice()->graphicsQueue, vk->getDevice()->logical);
+
+            ImGui_ImplVulkan_DestroyFontUploadObjects();
+        }
+
         /*
 		moduleManager.PushModule(new Log());
 		moduleManager.PushModule(new EventSystem::ShadowEventManager());
@@ -75,19 +176,18 @@ namespace ShadowEngine {
 
 	void ShadowApplication::Start()
 	{
+        object.createSingleRenderer(Geo::MeshType::Cube, glm::vec3(-1, 0, -1), glm::vec3(0.5));
+
         // Create the camera
         Camera camera {};
         camera.init(45, 1280, 720, 0.1, 10000);
         camera.setPosition(glm::vec3(0, 0, 4));
 
-        // Create the renderer
-        SingleRenderer object;
-        object.createSingleRenderer(Geo::MeshType::Cube, glm::vec3(-1, 0, -1), glm::vec3(0.5));
-
         SDL_Event event;
 		while (running)
 		{
             while (SDL_PollEvent(&event)) {  // poll until all events are handled!
+                ImGui_ImplSDL2_ProcessEvent(&event);
                 if (event.type == SDL_QUIT)
                     running = false;
             }
@@ -95,14 +195,32 @@ namespace ShadowEngine {
             object.setRotation(glm::rotate(object.getRotation(), (float) 0.5, glm::vec3(1, 0, 0)));
 
             VulkanManager::getInstance()->startDraw();
+            ImGui_ImplVulkan_NewFrame();
+            ImGui_ImplSDL2_NewFrame();
+            ImGui::NewFrame();
 
+            /** START OF RENDER AREA  */
+            /** PUT CODE HERE TO MAKE IT SHOW ON SCREEN */
             object.updateUniforms(camera);
             object.draw();
 
+            bool showDemo = true;
+            if (showDemo)
+                ImGui::ShowDemoWindow(&showDemo);
+            /** END OF RENDER AREA */
+            /** CODE AFTER HERE WILL NOT BE SHOWN ON SCREEN */
+            ImGui::Render();
+            ImGui_ImplVulkan_RenderDrawData(ImGui::GetDrawData(), VulkanManager::getInstance()->getCurrentCommandBuffer());
             VulkanManager::getInstance()->endDraw();
-
 
             Time::UpdateTime();
 		}
+
+        ImGui_ImplVulkan_Shutdown();
+        ImGui_ImplSDL2_Shutdown();
+        ImGui::DestroyContext();
+
+        SDL_DestroyWindow(window_->sdlWindowPtr);
+        SDL_Quit();
 	}
 }
