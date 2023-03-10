0009dc72850eb3a263a6276dc747674660eaf5db
Curle <curle@gemwire.uk>
2023-03-10 15:54:29 +0000

    File & FileSystem abstractions

---
+++ projs/shadow/shadow-engine/shadow-assets/src/str/string.cpp
@@ -0,0 +1,11 @@
+#include <str/string.h>
+
+namespace ShadowEngine::Str {
+    std::string toLower(const std::string& str) {
+        std::string temp;
+        for (auto c : str) {
+            temp.append(std::to_string(tolower(c)));
+        }
+        return temp;
+    }
+}
