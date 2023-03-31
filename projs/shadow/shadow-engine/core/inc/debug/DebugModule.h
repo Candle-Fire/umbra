865b86912ac34653ad2ed0d25062304d28e2d9bd
dpeter99 <dpeter99@gmail.com>
2023-03-31 13:47:22 +0200

    Remove old ids and dependeny functions

--- projs/shadow/shadow-engine/core/inc/debug/DebugModule.h
+++ projs/shadow/shadow-engine/core/inc/debug/DebugModule.h
@@ -8,34 +8,34 @@ namespace ShadowEngine::Debug {
 
     class DebugModule : public Module {
 
-        SHObject_Base(DebugModule)
+      SHObject_Base(DebugModule)
 
         bool active;
 
-    public:
-        DebugModule() : Module("module:/debug/debugger") {}
+      public:
+        DebugModule() : Module() {}
 
-        void Render(VkCommandBuffer& commands, int frame) override {};
+        void Render(VkCommandBuffer &commands, int frame) override {};
 
-        void PreInit() override {  };
+        void PreInit() override {};
 
-        void Init() override  {  };
+        void Init() override {};
 
         void Recreate() override {};
 
         void OverlayRender() override;
 
-        void Update(int frame) override {  };
+        void Update(int frame) override {};
 
-        void LateRender(VkCommandBuffer& commands, int frame) override {  };
+        void LateRender(VkCommandBuffer &commands, int frame) override {};
 
-        void AfterFrameEnd() override {  };
+        void AfterFrameEnd() override {};
 
-        void PreRender() override { };
+        void PreRender() override {};
 
         void Destroy() override {};
 
-        void Event(SDL_Event* e) override {};
+        void Event(SDL_Event *e) override {};
     };
 
 }
