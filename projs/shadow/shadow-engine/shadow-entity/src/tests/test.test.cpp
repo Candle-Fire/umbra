24317f97152327c347d102d84b2e755fef78e695
dpeter99 <dpeter99@gmail.com>
2023-04-10 21:00:39 +0200

    Basic test setup

--- projs/shadow/shadow-engine/shadow-entity/src/tests/test.test.cpp
+++ projs/shadow/shadow-engine/shadow-entity/src/tests/test.test.cpp
@@ -1,11 +1,22 @@
+
 #define CATCH_CONFIG_MAIN
-
 #include <catch2/catch.hpp>
-
 #include <cstdint>
+
+#include "NodeContainer.h"
 
 uint32_t factorial( uint32_t number ) {
     return number <= 1 ? number : factorial(number-1) * number;
+}
+
+using ShadowEngine::Entities::NodeContainer;
+
+class TestClass{
+
+};
+
+void a(){
+    NodeContainer<TestClass> container;
 }
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
